b76d6b19901ed541cfc27be1533bf8f08f12d8a2
MulacNo3 <jakov.stulic@gmail.com>
2019-11-07 21:50:25 +0100

    Kretanje i osnove izgleda. Dodani su neki objekti čisto kako bi imali izgled levela(staza, neprijatelj i baza), dodani su waypoints-i kojima se definira kretanje. Što se tiče skripti imamo skriptu koja sakuplja sve waypointse i stavlja ih u jednu od dvije liste. Te liste služe za kretanje neprijatelja koji se kreću od jedne točke do druge, kad dodu do zadnje točke se uništavaju. Za to sve je odgovorna skripta za kretanje neprijatelja.  LP. Mulac

---
+++ Guard the Shard/Assets/Waypoints.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Waypoints : MonoBehaviour
+{
+    //prvi put za neprijatelje
+    public static List<Transform> points1 = new List<Transform>();
+    //drugi put za neprijatelje
+    public static List<Transform> points2 = new List<Transform>();
+
+    void Awake()
+    {
+        // stavlja sve waypointe u listu
+
+        //samo da drži trenutni objekt
+        Transform current;
+        //iterira kroz svu djecu
+        for (int i = 0; i< transform.childCount; i++)
+        {
+            //postavlje i-to dijete u varijablu
+            current = transform.GetChild(i);
+            //stavlja se u listu po tome gdje pripada
+            if (current.CompareTag("Route1"))
+            {
+                points1.Add(current);
+            }
+            if (current.CompareTag("Route2"))
+            {
+                points2.Add(current);
+            }
+        }
+    }
+}
