05c79bd0d2b0e5a16eb383221dad7c6b05b0fbab
MulacNo3 <jakov.stulic@gmail.com>
2019-11-09 20:30:35 +0100

    Nešto oko kretanja dodano

---
+++ Guard the Shard/Assets/ClickArea.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickArea : MonoBehaviour
+{
+    GameObject obj;
+    Vector3 target;
+    void OnMouseDown()
+    {
+        if (MovingTurret.selected == true)
+        {
+            Debug.Log("Clicked the Collider!");
+            target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            target.z = transform.position.z;
+            obj.GetComponent<MovingTurret>().Movement(target);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        obj = GameObject.FindGameObjectWithTag("Turret");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Guard the Shard/Assets/MovingTurret.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingTurret : MonoBehaviour
+{
+    public float speed = 50f;
+    private Vector3 target;
+    public static bool selected = true;
+    void Start()
+    {
+        target = transform.position;
+    }
+    public void Movement(Vector3 target)
+    {
+        transform.position = Vector3.Lerp(transform.position, target, speed * Time.deltaTime);
+        selected = false;
+    }
+    void OnMouseDown()
+    {
+        Debug.Log("Selektirano!");
+        selected = true;
+    }
+    void Update()
+    {
+        
+    }
+}
