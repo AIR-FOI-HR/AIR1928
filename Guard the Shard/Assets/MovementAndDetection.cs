0aedc6a1a35bcb86414914c3e70396823ff34c19
MulacNo3 <jakov.stulic@gmail.com>
2019-11-09 23:50:24 +0100

    Polja, tap prepoznavanje,kolizije i teleportiranje

---
+++ Guard the Shard/Assets/MovementAndDetection.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementAndDetection : MonoBehaviour
+{
+    private MovementOfTurret script;
+    // Start is called before the first frame update
+    void Start()
+    {
+        script = GameObject.FindGameObjectWithTag("Turret").GetComponent<MovementOfTurret>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            Debug.Log("tap");
+            //script.Teleport();
+            
+            Vector2 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+            RaycastHit2D hit = Physics2D.Raycast(mouseWorldPos, Vector2.zero);
+            script.Teleport(hit);
+            /*
+            if (hit.collider != null)
+            {
+                Debug.Log("Hit Collider: " + hit.transform.name);
+            }
+            else
+            {
+                Debug.Log("No colliders hit from tap");
+            }
+            /*
+            Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                Debug.Log(hit.transform.gameObject.name);
+            }
+            /*
+            Ray raycast = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+            RaycastHit raycastHit;
+            if (Physics.Raycast(raycast, out raycastHit))
+            {
+                Debug.Log("Something Hit");
+                if (raycastHit.collider.name == "Turret")
+                {
+                    Debug.Log("Soccer Ball clicked");
+                }
+
+                //OR with Tag
+
+                if (raycastHit.collider.CompareTag("SoccerTag"))
+                {
+                    Debug.Log("Soccer Ball clicked");
+                }
+            }
+            */
+        }
+    }
+}
---
+++ Guard the Shard/Assets/MovementOfTurret.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementOfTurret : MonoBehaviour
+{
+    public bool selected = false;
+    Vector3 newPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        newPosition = transform.position;
+    }
+
+    public void Teleport(RaycastHit2D hit)
+    {
+        transform.position = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+        //transform.position = new Vector2(x, y);
+        //transform.position = new Vector3(hit.transform.position.x, hit.transform.position.y, transform.position.z);
+
+    } 
+    // Update is called once per frameInput.GetTouch(0).
+    void Update()
+    {
+        
+    }
+}
