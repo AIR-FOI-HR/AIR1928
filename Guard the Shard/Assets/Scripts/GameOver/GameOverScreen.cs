855b395c474873c929f43984f4bb9001dc6e4f70
Fabio Pera <fabio9779@gmail.com>
2019-11-12 23:33:13 +0100

    Dodan game over screen i osnovna navigacija s event listenerima za button clickove na screenu. Fali implementacija ispisa bodova, pokretanja nove igre.

---
+++ Guard the Shard/Assets/Scripts/GameOver/GameOverScreen.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameOverScreen : MonoBehaviour
+{
+    //Za potrebe testiranja se koristi property isGameOver, kasnije ce logika biti drugacije implementirana
+    bool isGameOver = true;
+    public Button PlayAgainButton;
+    public Button GoToMenuButton;
+    public GameObject PauseButton;
+    // Start is called before the first frame update
+    public GameObject gameOverScreenUi;
+    //Dodati event listenere
+    void Awake()
+    {
+        Button btnPlayAgain = PlayAgainButton.GetComponent<Button>();
+        btnPlayAgain.onClick.AddListener(PlayAgain);
+        Button btnGoToMenu = GoToMenuButton.GetComponent<Button>();
+        btnGoToMenu.onClick.AddListener(GoToMenu);
+    }
+    //Na početku igranja screen je neaktivan
+    void Start()
+    {
+  
+        gameOverScreenUi.SetActive(false);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Kada je igra gotovo zaustaviti igranje i prikazati game over screen -> optimizacija!
+        if (isGameOver)
+        {
+            gameOverScreenUi.SetActive(true);
+            Time.timeScale = 0f;
+            PauseButton.SetActive(false);
+            
+        }
+    }
+
+   void PlayAgain()
+    {
+        //Riješiti ponovo pokretanje scene -> transform error
+        //string currentSceneName = SceneManager.GetActiveScene().name;
+        //SceneManager.LoadScene(currentSceneName);
+
+
+    }
+
+    public void GoToMenu()
+    {
+        //kada main menu bude dodan dodati scene manager navigaciju
+    }
+}
