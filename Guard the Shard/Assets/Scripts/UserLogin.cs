655565aab6e1fbb36372a2a3e7be0a1b210b48d0
bayo04 <48364647+bayo04@users.noreply.github.com>
2019-11-13 01:41:15 +0100

    Dodana klasa za prijavu i registraciju

---
+++ Guard the Shard/Assets/Scripts/UserLogin.cs
@@ -0,0 +1,50 @@
+﻿using Firebase.Auth;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserLogin { 
+
+    FirebaseAuth Auth = FirebaseAuth.DefaultInstance;
+
+    public void SignUpUser(string email, string password)
+    {
+        Auth.CreateUserWithEmailAndPasswordAsync(email, password).ContinueWith(task => {
+            if (task.IsCanceled)
+            {
+                Debug.LogError("CreateUserWithEmailAndPasswordAsync was canceled.");
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                Debug.LogError("CreateUserWithEmailAndPasswordAsync encountered an error: " + task.Exception);
+                return;
+            }
+
+            // Firebase user has been created.
+            Firebase.Auth.FirebaseUser newUser = task.Result;
+            Debug.LogFormat("Firebase user created successfully: {0} ({1})",
+                newUser.DisplayName, newUser.UserId);
+        });
+    }
+
+    public void SignInUser(string email, string password)
+    {
+        Auth.SignInWithEmailAndPasswordAsync(email, password).ContinueWith(task => {
+            if (task.IsCanceled)
+            {
+                Debug.LogError("SignInWithEmailAndPasswordAsync was canceled.");
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                Debug.LogError("SignInWithEmailAndPasswordAsync encountered an error: " + task.Exception);
+                return;
+            }
+
+            Firebase.Auth.FirebaseUser newUser = task.Result;
+            Debug.LogFormat("User signed in successfully: {0} ({1})",
+                newUser.DisplayName, newUser.UserId);
+        });
+    }
+}
