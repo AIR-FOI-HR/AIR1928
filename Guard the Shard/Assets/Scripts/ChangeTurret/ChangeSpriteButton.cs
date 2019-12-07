13705ccbed4811ab5c1332ffbd548ff7f7a429be
Fabio Pera <fabio9779@gmail.com>
2019-12-07 15:59:08 +0100

    Dodana grafička reprezentacija turreta kojeg želimo zamijeniti u UI igre.

---
+++ Guard the Shard/Assets/Scripts/ChangeTurret/ChangeSpriteButton.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeSpriteButton : MonoBehaviour
+{
+    public Button changeButton;
+    public Sprite groundSprite;
+    public Sprite aerialSprite;
+
+    void Awake()
+    {
+        changeButton.onClick.AddListener(Change);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        changeButton.GetComponent<Image>().sprite = aerialSprite;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Change()
+    {
+        if (changeButton.GetComponent<Image>().sprite == aerialSprite)
+        {
+            changeButton.GetComponent<Image>().sprite = groundSprite;
+        }
+        else
+        {
+            changeButton.GetComponent<Image>().sprite = aerialSprite;
+        }
+    }
+}
