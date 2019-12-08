a136647b2a2a6724afef415df50b5d4f1559d410
MulacNo3 <jakov.stulic@gmail.com>
2019-12-08 13:05:13 +0100

    Implementacija prijenosa podataka u čitljiv oblik.
    Koristeći json se podatci prenose u Unity s web stranice napravljena je struktura po kojoj se isti prenose i sam web zahtijev.
    Sve je to smješteno u novi objekt koji sadržava tu skriptu te će služiti za daljnje slanje podataka ostalim objektima kad za to bude potrebe.
    Podatci koje sadržava su : informacije za turret , lista svih neprijatelja na nekom levelu i njihove osobine, poredak u kojem neprijatelji izlaze i u kojem točno valu izlaze.
    
    LP. Treći Mulac

---
+++ Guard the Shard/Assets/Scripts/Database/Waves/AllLevelData.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+[Serializable]
+public class AllLevelData
+{
+    public int TurID;
+    public float TurAttSp;
+    public float BullSpeed;
+    public string TurPrefab;
+    public float TurDmgRange;
+    public float TurDamage;
+    public List<EnemyListData> enemies;
+    public List<WaveData> waves;
+}
+
+[Serializable]
+public class EnemyListData
+{
+    public int EneID;
+    public int EneHP;
+    public string EnePrefabName;
+    public char EneType;
+    public float EneSpeed;
+    public int EneWorth;
+}
+
+[Serializable]
+public class WaveData
+{
+    public int LevelNum;
+    public int Enemy;
+    public int Number;
+    public int TimerAfterWave;
+}
