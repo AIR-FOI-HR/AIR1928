efdab70ecb25431bb4ba31664aaa02367fc4e9bd
MulacNo3 <jakov.stulic@gmail.com>
2019-12-10 16:33:48 +0100

    Prilagodba za story mode
    Što je napravljeno : kopiran je Endless wave spawner i prilagođen tome da se ispisuje lista.
    lista je ispisuje u cijelosti za ovaj spawner , lista se odnosi na listu neprijatelja sa servera.
    
    Što još treba : prebrojavati neprijatelje i ne puštati nove dok se jedan wave ne ubije u cijelosti .
    postaviti neki uvjet kako bi znali kada je kraj tog level tj. zadnji val je pobijeđen u cijelosti.

---
+++ Guard the Shard/Assets/Scripts/EnemyStorySpawner.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStorySpawner : MonoBehaviour
+{
+    //Lokacija izlaska neprijatelja
+    public Transform spawnPoint;
+    public Transform spawnPoint2;
+    //vrijeme između valova
+    public float timeBetweenWaves = 5f;
+    //2 sec do prvog
+    private float countdown = 2f;
+    //broj vala
+    public int waveNumber = 0;
+    //pozicija u listi neprijatelja
+    public int currentListPos = 0;
+    //skripta u kojoj se nalaze podatci levela
+    public LevelDataControler scriptWithData;
+    //svi neprijatelji koji se pojavljuju u levelu 
+    public List<GameObject> enemyPrefabList;
+    //odglumi pointera za prefab koji trebamo
+    public List<int> pointerLikeList;
+    //Lista neprijatelja po valovima
+    public List<List<int>> ListofEnemies = new List<List<int>>();
+    //postavljanje parametara i liste neprijatelja
+    private void Start()
+    {
+        scriptWithData = GameObject.Find("DataHandler").GetComponent<LevelDataControler>();
+        foreach (EnemyListData item in scriptWithData.allLevelData.enemies)
+        {
+            enemyPrefabList.Add(Resources.Load(item.EnePrefabName) as GameObject);
+            pointerLikeList.Add(item.EneID);
+        }
+        //privremena lista
+        List<int> placeholderList = new List<int>();
+        //prvi i najmanji idex
+        int index = scriptWithData.allLevelData.waves[0].LevelNum;
+        //samo stavljanje u listu koja sadrži sve valove individualne valove koji su naponjeni
+        foreach (WaveData item in scriptWithData.allLevelData.waves)
+        {
+            if (index != item.LevelNum)
+            {
+                ListofEnemies.Add(placeholderList);
+                placeholderList = new List<int>();
+                index = item.LevelNum;
+            }
+            if (index == item.LevelNum)
+            {
+                placeholderList.Add(item.Enemy);
+            }
+        }
+        ListofEnemies.Add(placeholderList);
+    }
+    private void Update()
+    {
+        //ako je odbrojavanje došlo do kraja
+        if (countdown <= 0)
+        {
+            //Pošto se radi o storymode-u ako je došlo do kraj lista s valovima ništa se ne događa
+            if (ListofEnemies.Count < (waveNumber+1)) return;
+            //stvori wave
+            StartCoroutine(SpawnWave());
+            //reset timera
+            countdown = timeBetweenWaves + waveNumber;
+        }
+        //odbrojavanje
+        countdown -= Time.deltaTime;
+    }
+    IEnumerator SpawnWave()
+    {
+        //povećanje broj wavea za 1
+        waveNumber++;
+        //pozicija u listi neprijatelja za dani val se resetira
+        currentListPos = 0;
+        //stvareanje waveNumber neprijatelja
+        for (int i = 0; i < (ListofEnemies[waveNumber-1].Count); i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.5f);
+        }
+        //Debug.Log("Wave incoming");
+    }
+    void SpawnEnemy()
+    {
+        //samo spawnanje neprijatelja svaki neprijatelj u valu ima istu šansu za spawn (od onog očitanog iz baze)
+        int indexer = 0;
+        foreach (int item in pointerLikeList)
+        {
+            if (item == ListofEnemies[waveNumber - 1][currentListPos])
+            {
+                //gdje je spawn point 1 ili 2
+                switch (Random.Range(0, 2))
+                {
+                    case 0:
+                        GameObject instantiatedOnPath1 = (GameObject)Instantiate(enemyPrefabList[indexer], spawnPoint.position, spawnPoint.rotation);
+                        NeprijateljKretanje onPath1 = instantiatedOnPath1.GetComponent<NeprijateljKretanje>();
+                        //ako ista postoji
+                        if (onPath1 != null)
+                        {
+                            onPath1.GetParameters(scriptWithData.allLevelData.enemies[indexer].EneSpeed, 1);
+                        }
+                        //ostali parametri
+                        NeprijateljFunction onPath1Fun = instantiatedOnPath1.GetComponent<NeprijateljFunction>();
+                        //ako ista postoji
+                        if (onPath1Fun != null)
+                        {
+                            onPath1Fun.GetParameters(scriptWithData.allLevelData.enemies[indexer].EneHP, scriptWithData.allLevelData.enemies[indexer].EneWorth);
+                        }
+                        break;
+                    case 1:
+                        GameObject instantiatedOnPath2 = (GameObject)Instantiate(enemyPrefabList[indexer], spawnPoint2.position, spawnPoint2.rotation);
+                        //prijenos parametara skripti za kretanje
+                        NeprijateljKretanje onPath2 = instantiatedOnPath2.GetComponent<NeprijateljKretanje>();
+                        //ako ista postoji
+                        if (onPath2 != null)
+                        {
+                            onPath2.GetParameters(scriptWithData.allLevelData.enemies[indexer].EneSpeed, 2);
+                        }
+                        //ostali parametri
+                        NeprijateljFunction onPath2Fun = instantiatedOnPath2.GetComponent<NeprijateljFunction>();
+                        //ako ista postoji
+                        if (onPath2Fun != null)
+                        {
+                            onPath2Fun.GetParameters(scriptWithData.allLevelData.enemies[indexer].EneHP, scriptWithData.allLevelData.enemies[indexer].EneWorth);
+                        }
+                        break;
+                }
+                break;
+            }
+            indexer++;
+        }
+        //povećanje current list pos tj pozicije za jedan kako bi znali kojeg neprijatelja stvoriti
+        currentListPos++;
+    }
+}
