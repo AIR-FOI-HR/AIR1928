221a6afd2e254630759855082ae398e45db82657
Zvonimir Sorić <zsoric@foi.hr>
2019-12-14 11:28:01 +0100

    6.1 Generiranje tekstualnih okvira

---
+++ Guard the Shard/Assets/Scripts/DialogueMenager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueMenager : MonoBehaviour
+{
+    public Queue<string> sentences;
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+}
