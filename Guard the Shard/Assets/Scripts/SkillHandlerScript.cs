9e068d670b6610d58d79808fa644e04bc2ca9fd0
MulacNo3 <jakov.stulic@gmail.com>
2019-12-17 13:21:30 +0100

    MAle izmjene nakon konzultacija
    Promjene u skill handleru većinski ne funkcionale, promjene komentara da bi bilo jasnije i nepotrebni komentari su maknuti.
    
    LP treći mulac.

--- Guard the Shard/Assets/Scripts/SkillHandlerScript.cs
+++ Guard the Shard/Assets/Scripts/SkillHandlerScript.cs
@@ -6,7 +6,6 @@ public class SkillHandlerScript : MonoBehaviour
 {
     // Ova klasa služi upravljanju vještinama , to uključuje više stvari počevši od samog dohvaćanja podataka preko instanciranja  do pozivanja instanciranih vještina
 
-    //zadnja pozicija klika/tap koja je nužna kako i znali gdje se navedeni skil treba baciti tj. aktivirati ; prosljedi se handleru za skill
     public Vector3 LastClickPosition;
     //0 za prvi skill , 1 za drugi itd ; ovo služi kako bi znali koji skill pozvati
     public int skillToUSe = 0;
@@ -35,30 +34,20 @@ public class SkillHandlerScript : MonoBehaviour
 
     void Start()
     {
-        //dohvaćanje skripte s podatcima
         scriptWithData = GameObject.Find("DataHandler").GetComponent<LevelDataControler>();
-        //poziv funkcije za učitavanje vještina u listu
         GetModules();
     }
     //učitavanje vještina u listu i prosljeđivanje parametara
     void GetModules()
     {
-        //index u listi
         int index = 0;
-        //za svaki dohvaćeni skill se odradi sljedeće
         foreach (SkillData item in scriptWithData.allLevelData.skill)
         {
-            //loada se prefab u privremeni objekt
             GameObject objectToSpawn = Resources.Load(item.PrefabName) as GameObject;
-            //Debug.Log(item.PrefabName);
-            //if (objectToSpawn == null) Debug.Log("Nothing");
-            //instancira se učitani objekt
             GameObject target = Instantiate(objectToSpawn, new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
-            //dodaje se klasa z a upravljanje objektom koja nasljeđuje interface u listu kako bi se kasnije pozivalo
+            //dodaje se klasa za upravljanje objektom koja nasljeđuje interface u listu kako bi se kasnije pozivalo
             ListOfSkills.Add((ISkillInterface)target.GetComponent(typeof(ISkillInterface)));
-            //prosljeđivanje parametara koji su očitani iz baze toj klasi
             ListOfSkills[index].SendParameters(item.RangeSkill, item.Damage, item.SlowSkill, item.Duration);
-            //prelazak na sljedeći index u listi
             index++;
         }
     }
@@ -71,12 +60,9 @@ public class SkillHandlerScript : MonoBehaviour
             //raycasting kako bi dohvatit sve udarce u collidere
             Vector2 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
             RaycastHit2D[] hits = Physics2D.RaycastAll(mouseWorldPos, Vector2.zero);
-            //skill usable area nije selektiran
             if (RaycastElemnt("SkillArea", hits) && Enabled)
             {
-                //aktivacija skill
                 ListOfSkills[skillToUSe].SpawnObject(LastClickPosition);
-                //onemogućavanje korištenja skilla
                 Enabled = false;
             }
         }
