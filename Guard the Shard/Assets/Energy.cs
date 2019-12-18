fed18f8f2c531dd7427a4e5654e08a66b8186f1e
MulacNo3 <jakov.stulic@gmail.com>
2019-12-18 11:39:24 +0100

    Sakupljanje energije gotovo.
    Dodan Energy container koji drži ukupnu energiju i ima funkcije za dodavanje energije.
    Svakih 5 sekundi kreira jedan kristal na koji se može kliknuti. On time nestaje i nadodaje se 10 energije koja se može koristiti za vještine.
    Također je kreiran i prefab za kristal koji se još treba samo zamjeniti spriteom zbog izgleda tj funkcionalno je gotov,
    
    LP Mulac no 3.

---
+++ Guard the Shard/Assets/Energy.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Energy : MonoBehaviour
+{
+    public ScaleEnergy EnergyBarScript = null;
+    public GameObject crystal;
+    public float currentEnergy = 0;
+    private float countdown = 5f;
+    private void Start()
+    {
+        EnergyBarScript = GameObject.Find("EnergyBar").GetComponent<ScaleEnergy>();
+    }
+    private GameObject RaycastElemnt(string name, RaycastHit2D[] hits)
+    {
+        foreach (var item in hits)
+        {
+            if (item.collider.name == name)
+            {
+                return item.collider.gameObject;
+            }
+        }
+        return null;
+    }
+    void Update()
+    {
+        if (countdown <= 0)
+        {
+            //stvori kristal
+            Instantiate(crystal,Random.insideUnitCircle*4, Quaternion.identity);
+            //reset timera
+            countdown = 5f;
+        }
+        //odbrojavanje
+        countdown -= Time.deltaTime;
+        if (Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            //Debug.Log("tap");
+            //raycasting
+            Vector2 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+            RaycastHit2D[] hits = Physics2D.RaycastAll(mouseWorldPos, Vector2.zero);
+            Debug.Log(hits.Length);
+            GameObject crystal = RaycastElemnt("Crystal(Clone)",hits);
+            if (crystal != null)
+            {
+                Destroy(crystal);
+                AddEnergy(10);
+            }
+        }
+    }
+    public void AddEnergy(float amount)
+    {
+        if((currentEnergy + amount) > 100)
+        {
+            currentEnergy = 100;
+        }
+        else
+        {
+            currentEnergy = currentEnergy + amount;
+        }
+        EnergyBarScript.Scale(currentEnergy);
+    }
+}
