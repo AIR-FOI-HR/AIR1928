2881432554c0e26f786feccee275787bddb82b23
MulacNo3 <jakov.stulic@gmail.com>
2020-01-25 20:01:51 +0100

    Programski kod iza tutorijala odvija se u više faza.
    1. Pomaknite turret
    2. Promjenite tip turreta
    3. Skupite energiju
    4. Ubijte neprijatelje
    5. Iskoristite vještinu na meti
    
    To je izvedeno manipulacijom već izrađenih dijelova u kombinaciji s novim kodom,recimo neprijateljima je oduzeta komponenta za kretanje, energiju nije potrebno skupljati nakon prvog puta niti se kristali pojavljuju jer se countdown postavlja na infinity, wave spawneri se isključuju umjesto njih su neprijatelji već postavljeni na željene pozicije ali ugašeni te se po potrebi pale (znamo da su to oni jer pripadaju EnemyParent objektu koji služi samo kako bi se mogli pronaći jer Find ne radi na isključenim objektima).
    
    LP treći mulac

---
+++ Guard the Shard/Assets/TutorijalManager.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorijalManager : MonoBehaviour
+{
+    public int stage = 0;
+    private Vector3 initial;
+    private GameObject turret;
+    private Energy energyscript;
+    private float energy = 0;
+    private string type = "ground";
+    // Start is called before the first frame update
+    void Start()
+    {
+        stage = 0;
+        turret = GameObject.Find("Turret");
+        initial = turret.transform.position;
+        energyscript = GameObject.Find("EnergyContainer").GetComponent<Energy>();
+    }
+    void Update()
+    {
+        switch (stage)
+        {
+            case 0:
+                if (turret.transform.position != initial)
+                {
+                    Debug.Log("turret pomaknut :)");
+                    stage++;
+                    type = turret.GetComponent<TurretAttacking>().type;
+                }
+                break;
+            case 1:
+                if (type != turret.GetComponent<TurretAttacking>().type)
+                {
+                    Debug.Log("tip turreta promijenjen");
+                    stage++;
+                    energy = energyscript.currentEnergy;
+                }
+                break;
+            case 2:
+                if(energy != energyscript.currentEnergy)
+                {
+                    Debug.Log("energija pokupljena/potrošena");
+                    energyscript.countdown = Mathf.Infinity;
+                    GameObject crystal = GameObject.Find("Crystal");
+                    while (crystal != null)
+                    {
+                        GameObject.Destroy(crystal);
+                        crystal = GameObject.Find("Crystal");
+                    }
+                    GameObject.Find("EnemyParent").transform.Find("AirDummy").gameObject.SetActive(true);
+                    GameObject.Find("EnemyParent").transform.Find("EarthDummy").gameObject.SetActive(true);
+                    stage++;
+                }
+                break;
+            case 3:
+                //da ne ubiju neprijatelje vještinama
+                if (energyscript.currentEnergy != 0)
+                {
+                    energyscript.Deduct((int)energyscript.currentEnergy);
+                }
+                if (GameObject.Find("AirDummy") == null && GameObject.Find("EarthDummy") == null)
+                {
+                    Debug.Log("neprijatelji ubijeni");
+                    turret.SetActive(false);
+                    GameObject.Find("EnemyParent").transform.Find("AbilityDummy").gameObject.SetActive(true);
+                    energyscript.AddEnergy(100);
+                    stage++;
+                }
+                break;
+            case 4:
+                if(energyscript.currentEnergy < 100)
+                {
+                    energyscript.AddEnergy(100 - energyscript.currentEnergy);
+                }
+                if (GameObject.Find("AbilityDummy") == null)
+                {
+                    Debug.Log("vještina iskorištena ispravno, tutorijal gotov");
+                    stage++;
+                }
+                break;
+        }
+    }
+
+}
