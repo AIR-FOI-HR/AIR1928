456c3ce71e571c52de4f27ce98b63a5621c1684f
Zoran <zzovko@foi.hr>
2020-01-30 01:35:59 +0100

    Omogućena korisniku promijena lozinke nakon upisa stare lozinke, nove lozinke i ponovljene nove lozinke

---
+++ Guard the Shard/Assets/Scripts/ChangePassword.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ChangePassword : MonoBehaviour
+{
+    public InputField inpOldPassword;
+    public InputField inpNewPassword;
+    public InputField inpNewPasswordAgain;
+
+    private UserControl userControl = new UserControl();
+
+    public void ConfirmNewPasword()
+    {
+        if (inpNewPassword.text.ToString() == inpNewPasswordAgain.text.ToString())
+        {
+            if (userControl.ChangePassword(PlayerPrefs.GetInt("userid"), inpOldPassword.text, inpNewPassword.text))
+            {
+                SceneManager.LoadScene(7);
+            }
+            else
+            {
+
+            }
+        }
+        else
+        {
+
+        }
+    }
+
+    public void Izlaz()
+    {
+        SceneManager.LoadScene(7);
+    }
+}
